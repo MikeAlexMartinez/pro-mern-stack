c7ab7a875b291e3fbe6f51919837d0f69916e0a5
root <mike.alex.martinez@gmail.com>
2017-04-05 22:19:48 +0100

    Completed code upto hot module replacement

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const MongoClient = require('mongodb').MongoClient;
+
+const Issue = require('./issue.js');
+
 // define db in global scope.
 let db;
 
@@ -17,40 +19,6 @@ app.use(bodyParser.json());
 
 /* enable strong etags */
 app.enable('etag');
-
-const validIssueStatus = {
-    New: true,
-    Open: true,
-    Assigned: true,
-    Fixed: true,
-    Verified: true,
-    Closed: true
-};
-
-const issueFieldType = {
-    status: 'required',
-    owner: 'required',
-    effort: 'optional',
-    created: 'required',
-    completionDate: 'optional',
-    title: 'required'
-};
-
-function validateIssue(issue) {
-    for(const field in issueFieldType) {
-        const type = issueFieldType[field];
-        if(!type) {
-            delete issue[field];
-        } else if (type === "required" && !issue[field]) {
-            return `${field} is required.`;
-        }
-    }
-
-    if(!validIssueStatus[issue.status])
-        return `${issue.status} is not a valid status.`;
-
-    return null;
-}
 
 app.get('/api/issues', (req, res) => {
     console.log(req.method + ": " + req.url + ", " + req.headers["user-agent"]);
@@ -74,7 +42,7 @@ app.post('/api/issues', (req, res) => {
         newIssue.status = 'New';
     }
 
-    const err = validateIssue(newIssue);
+    const err = Issue.validateIssue(newIssue);
     if(err) {
         console.log(err);
         res.status(422).json({ message: `Invalid request: ${err}` });
